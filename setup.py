b1fb19180f3ee10be88aa106abd5885c0cdb2e79
Matt Dennewitz <mattdennewitz@gmail.com>
2015-02-01 23:47:05 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+from pip.req import parse_requirements
+
+
+reqs_txt = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+pip_reqs = [unicode(obj.req) for obj in parse_requirements(reqs_txt)]
+
+setup(
+    name='baseball-projection-schematics',
+    version='0.1.0',
+    description='Translates projection data into a unified schema',
+    author='Matt Dennewitz',
+    author_email='mattdennewitz@gmail.com',
+    url='https://github.com/mattdennewitz/baseball-projection-schematics',
+
+    install_requires=pip_reqs,
+
+    packages=find_packages(),
+
+    scripts=[
+        'scripts/bb-generate-schematic',
+        'scripts/bb-process-projections',
+    ],
+)
